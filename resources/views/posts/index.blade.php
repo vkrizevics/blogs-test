22f663eea93c893f702591c71e9ea1e8b4c4925b
vkrizevics <vkrizevics@gmail.com>
2024-10-07 14:54:49 +0300

    BT1 - Blogs test task - styling CRUD links in posts and comments

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -22,23 +22,37 @@
                     <div>
                         <a href="{{ route('posts.show', ['post' => $post->id]) }}"
                            class="underline underline-offset-2 hover:text-sky-500">{{ $post->comments()->count() }} comments</a>
+                    </div>
 
-                        <a href="{{ route('posts.comments.create', ['post' => $post->id]) }}"
-                           class="underline underline-offset-2 hover:text-sky-500">Add a comment</a>
-
-                        @if (Auth::check() && $post->user->id = Auth::id())
+                    @if (Auth::check() && $post->user->id = Auth::id())
+                        <div>
                             <a href="{{ route('posts.edit', ['post' => $post->id]) }}"
-                               class="underline underline-offset-2 hover:text-sky-500">Edit the post</a>
+                               class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md
+                                  font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700
+                                  focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2
+                                  focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Edit</a>
 
-                            <a href="#destroy" class="underline underline-offset-2 hover:text-sky-500"
-                               onclick="document.getElementById('delete-form-{{$post->id}}').submit();">Delete the post</a>
+                            <a href="#destroy" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md
+                                  font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700
+                                  focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2
+                                  focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                               onclick="document.getElementById('delete-form-{{$post->id}}').submit();">Delete</a>
 
                             <form id="delete-form-{{$post->id}}" method="post"
                                   action="{{ route('posts.destroy', ['post' => $post->id]) }}" class="hidden">
                                 @csrf
                                 @method('delete')
                             </form>
-                        @endif
+
+                            <a href="{{ route('posts.comments.create', ['post' => $post->id]) }}"
+                               class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md
+                                  font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700
+                                  focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2
+                                  focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Comment</a>
+
+                        </div>
+                    @endif
+
                     </div>
                 </div>
             </div>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -17,31 +17,46 @@
                     {!! nl2br(htmlspecialchars($post->content), false) !!}
                 </div>
 
+                @if (Auth::check() && $post->user->id = Auth::id())
+                    <div>
+                        <a href="{{ route('posts.edit', ['post' => $post->id]) }}"
+                           class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md
+                              font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700
+                              focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2
+                              focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Edit</a>
+
+                        <a href="#destroy" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md
+                              font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700
+                              focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2
+                              focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                           onclick="document.getElementById('delete-form-{{$post->id}}').submit();">Delete</a>
+
+                        <form id="delete-form-{{$post->id}}" method="post"
+                              action="{{ route('posts.destroy', ['post' => $post->id]) }}" class="hidden">
+                            @csrf
+                            @method('delete')
+                        </form>
+
+                        <a href="{{ route('posts.comments.create', ['post' => $post->id]) }}"
+                           class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md
+                              font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700
+                              focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2
+                              focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Comment</a>
+                    </div>
+                @endif
+
                 <div>
                     {{ count($post->comments) }} comments
-                    <a href="{{ route('posts.comments.create', ['post' => $post->id]) }}"
-                       class="underline underline-offset-2 hover:text-sky-500">Add a comment</a>
-
-                    @if (Auth::check() && $post->user->id = Auth::id())
-
-                            <a href="{{ route('posts.edit', ['post' => $post->id]) }}"
-                               class="underline underline-offset-2 hover:text-sky-500">Edit the post</a>
-
-                            <a href="#destroy" class="underline underline-offset-2 hover:text-sky-500"
-                               onclick="document.getElementById('delete-form-{{$post->id}}').submit();">Delete the post</a>
-
-                            <form id="delete-form-{{$post->id}}" method="post"
-                                  action="{{ route('posts.destroy', ['post' => $post->id]) }}" class="hidden">
-                                @csrf
-                                @method('delete')
-                            </form>
-                    @endif
                 </div>
 
                 @foreach($post->comments as $comment)
                     <div>
                         {{ $comment->content }}
-                        <a href="#destroy-comment" class="underline underline-offset-2 hover:text-sky-500 float-right"
+                        <a href="#destroy-comment"
+                           class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md
+                              font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700
+                              focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2
+                              focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 float-right"
                            onclick="document.getElementById('delete-form-comment-{{$comment->id}}').submit();">X</a>
 
                         <form id="delete-form-comment-{{$comment->id}}" method="post"
