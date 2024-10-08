961524c39a0ead3a9d4a37eaf194a6999ea0fb54
vkrizevics <vkrizevics@gmail.com>
2024-10-08 12:29:35 +0300

    BT1 - Categories and Categories / Posts pivot CRUDs

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,6 +7,7 @@ use DateTime;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -27,5 +28,10 @@ class Post extends Model
     public function comments(): HasMany
     {
         return $this->hasMany(Comment::class);
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class, 'category_post')->withTimestamps();
     }
 }
