369c3a4e35ff4f114ac022b6c79298f25b73054e
vkrizevics <vkrizevics@gmail.com>
2024-10-18 19:00:39 +0300

    I-1: Inertia - Search form debugging

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -81,7 +81,8 @@ const search = () => {
                         placeholder="keywords"
                         @keyup.enter="search"
                     />
-                    <Link :href="route('posts.search', {fragment: form.keywords})"
+                    <Link href="#search"
+                          @click.prevent="search"
                           class="inline-flex items-center mr-2 px-4 py-2 bg-gray-800 border border-transparent rounded-md
                                       font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700
                                       focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2
