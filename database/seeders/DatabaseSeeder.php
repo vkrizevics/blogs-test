4a7f500147e461ce6e11171b8dbeb414e5371bd1
vkrizevics <vkrizevics@gmail.com>
2024-10-20 16:31:45 +0300

    S-1: Randomizing authors of the recent posts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,35 +16,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $usersWithPosts = User::factory(10)
-            ->hasPosts(random_int(1, 20))
+        $users_with_posts = User::factory(10)
             ->create();
 
-        $userWithoutPosts = User::factory(2)
+        $users_without_posts = User::factory(random_int(2, 10))
             ->create(); // with no posts
 
-        $allUsers = [$usersWithPosts, $userWithoutPosts];
+        $users_for_posts = [];
+        foreach ($users_with_posts as $user) {
+            $has_n_posts = random_int(1, 20);
+            for ($n = 0; $n < $has_n_posts; $n++) {
+                $users_for_posts[] = $user;
+            }
+        }
+
+        $users_for_posts_randomized = Arr::shuffle($users_for_posts);
+        foreach ($users_for_posts_randomized as $user_p_r) {
+            Post::factory(1)
+                ->for($user_p_r)
+                ->create();
+        }
+
         $posts = Post::all();
+        $all_users = [$users_with_posts, $users_without_posts];
         foreach ($posts as $post) {
             $comments_needed = random_int(0, 3);
             if (!$comments_needed) {
                 continue;
             }
 
-            $usersNeeded = [];
-            foreach ($allUsers as $usersPart) {
-                foreach ($usersPart as $user) {
+            $users_needed = [];
+            foreach ($all_users as $users_part) {
+                foreach ($users_part as $user) {
                     $per_user = random_int(0, 3);
                     for ($i = 0; $i < $per_user; $i++) {
-                        $usersNeeded[] = $user;
+                        $users_needed[] = $user;
                     }
                 }
             }
 
-            $usersRandomized = Arr::shuffle($usersNeeded);
-            foreach ($usersRandomized as $userR) {
+            $users_randomized = Arr::shuffle($users_needed);
+            foreach ($users_randomized as $user_r) {
                 Comment::factory(1)
-                    ->for($userR)
+                    ->for($user_r)
                     ->for($post)
                     ->create();
             }
