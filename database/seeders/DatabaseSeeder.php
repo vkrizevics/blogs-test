a4233736ff3851fd04453e4e11759eafc051060a
vkrizevics <vkrizevics@gmail.com>
2024-10-20 22:18:18 +0300

    S-1: Meaningful categories for the posts seeded

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
@@ -11,6 +12,22 @@ use Illuminate\Support\Arr;
 
 class DatabaseSeeder extends Seeder
 {
+    const CATEGORIES_IN_TEXT = [
+        'alice',
+        'carroll',
+        'whiterabbit',
+        'hatter',
+        'mockturtle',
+        'dormouse',
+        'beautifulsoup'
+    ];
+
+    const CATEGORIES_OUTER = [
+        'lewiscarroll',
+        'aliceinthewonderland',
+        'aliceliddell',
+    ];
+
     /**
      * Seed the application's database.
      */
@@ -61,6 +78,45 @@ class DatabaseSeeder extends Seeder
                     ->for($user_r)
                     ->for($post)
                     ->create();
+            }
+
+            $category_ids_by_names = [];
+            foreach (static::CATEGORIES_IN_TEXT as $category_name) {
+                $category = new Category;
+                $category->name = $category_name;
+                $category->save();
+
+                $category_ids_by_names[$category->name] = $category->id;
+            }
+
+            foreach (static::CATEGORIES_OUTER as $category_name_outer) {
+                $category = new Category;
+                $category->name = $category_name_outer;
+                $category->save();
+
+                $category_ids_by_names[$category->name] = $category->id;
+            }
+
+            foreach ($posts as $post) {
+                $post_to_look_for_categories = mb_strtolower(
+                    str_replace(' ', '', $post->title . $post->content)
+                );
+
+                $post_categories = [];
+                foreach (static::CATEGORIES_IN_TEXT as $category_name_searched_for) {
+                    if (strpos($post_to_look_for_categories, $category_name_searched_for) !== false) {
+                        $post_categories[] = $category_ids_by_names[$category_name_searched_for];
+                    }
+                }
+
+                $categories_outer = Arr::shuffle(static::CATEGORIES_OUTER);
+                $n_outer = random_int(0, count($categories_outer));
+                for ($i_outer = 1; $i_outer <= $n_outer; $i_outer++) {
+                    $category_name_assigning = $categories_outer[$i_outer - 1];
+                    $post_categories[] = $category_ids_by_names[$category_name_assigning];
+                }
+
+                $post->categories()->sync($post_categories);
             }
         }
     }
