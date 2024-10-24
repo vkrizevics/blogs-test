20a96d3ba26336216705e8f1eeda956f4f63f812
vkrizevics <vkrizevics@gmail.com>
2024-10-24 11:57:28 +0300

    S-1: Post access control - customized error messages

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -35,17 +35,21 @@ class PostPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Post $post): bool
+    public function update(User $user, Post $post): Response
     {
-        return $post->user_id === $user->id;
+        return $post->user_id === $user->id
+            ? Response::allow()
+            : Response::deny('You do not own this post.');
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Post $post): bool
+    public function delete(User $user, Post $post): Response
     {
-        return $post->user_id === $user->id;
+        return $post->user_id === $user->id
+            ? Response::allow()
+            : Response::deny('You do not own this post.');
     }
 
     /**
